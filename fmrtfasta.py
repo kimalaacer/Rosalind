b7fa7878d5058d0106a3c7c333f83773a614ab13
kimalaacer <merouehchady@gmail.com>
2019-07-30 05:39:55 -0500

    Update fmrtfasta.py

--- fmrtfasta.py
+++ fmrtfasta.py
@@ -1,5 +1,5 @@
 from Bio import Entrez
-Entrez.email = "merouehchady@gmail.com"
+Entrez.email = "XXXXXXX@gmail.com"
 handle = Entrez.efetch(db="nucleotide", id=["NM_013179"], rettype="fasta")
 records = handle.read()
 print records
